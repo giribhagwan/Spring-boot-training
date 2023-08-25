596e0c51c76fb4734ff69854628af555e46b232c
Bhagwan <272865@ODCDPW.IC>
2023-08-25 19:13:23 +0530

    session5

---
+++ session5/src/main/java/com/intech/session5/controller/MyTaskManagerController.java
@@ -0,0 +1,31 @@
+package com.intech.session5.controller;
+
+import com.intech.session5.dmain.MyTask;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/task")
+public class MyTaskManagerController {
+
+    List<MyTask> taskList=new ArrayList<>(Arrays.asList(
+            new MyTask("title1","description1"),
+            new MyTask("title2","description2"),
+            new MyTask("title3","description3"),
+            new MyTask("title4","description4"),
+            new MyTask("title5","description5")
+    ));
+    @GetMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<MyTask>> getAll(){
+        return new ResponseEntity<>(taskList, HttpStatus.OK);
+    }
+}
---
+++ session5/src/main/java/com/intech/session5/dmain/MyTask.java
@@ -0,0 +1,11 @@
+package com.intech.session5.dmain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MyTask {
+    String title;
+    String description;
+}
